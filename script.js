8bcdb5ca83da9d701bf923b07db1cf8d4eec813d
potatoland4492 <96445998+potatoland4492@users.noreply.github.com>
2022-01-11 08:00:29 -0800

    update css?

---
+++ script.js
@@ -0,0 +1,5 @@
+let joinbtn = document.body.getElementById('add-bot-button'); // document.getElementById() ?
+
+joinbtn.addEventListener('click', {
+  window.open('https://discord.com/'). // Edit URL
+}) // ?
