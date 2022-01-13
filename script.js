06f3a1812c50506fe38736bd7ae27aa823162828
potatoland4492 <potatoland4492@gmail.com>
2022-01-12 17:42:54 -0800

    Fix Discord links

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let addbtn = document.getElementById('add-bot-button');
 addbtn.addEventListener('click', function(){
-	window.open('https://discord.com/oauth2/'); // Edit bot invite URL
+	window.open('https://discord.com/api/oauth2/authorize?client_id=915726505663938621&permissions=277092682944&scope=bot%20applications.commands');
 });
 
 let joinbtn = document.getElementById('join-server-button');
 joinbtn.addEventListener('click', function(){
-	window.open('https://discord.gg/'); // Edit server invite URL
+	window.open('https://discord.com/invite/6DSKgJXtFH');
 });
