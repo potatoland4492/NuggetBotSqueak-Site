f7b833d4a6b4bd20093be95c46113167d3283039
potatoland4492 <96445998+potatoland4492@users.noreply.github.com>
2022-01-12 16:37:24 -0800

    changed content, fixed Features

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let addbtn = document.getElementById('add-bot-button');
 addbtn.addEventListener('click', function(){
-  window.open('https://discord.com/oauth2/'); // Edit bot invite URL
+	window.open('https://discord.com/oauth2/'); // Edit bot invite URL
 });
 
 let joinbtn = document.getElementById('join-server-button');
 joinbtn.addEventListener('click', function(){
-  window.open('https://discord.gg/'); // Edit server invite URL
+	window.open('https://discord.gg/'); // Edit server invite URL
 });
